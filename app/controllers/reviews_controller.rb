c8874dc18b99325edd805430dd7da080207b17ef
Pierre Richemond <pierreambroise@hotmail.fr>
2021-08-11 16:59:33 +0900

    challenge 2 done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.create(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    if @review.valid?
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
